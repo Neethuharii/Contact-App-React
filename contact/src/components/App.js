7d57752587766f5b98517824616b03fb5a9f3fd0
Neethu Hari <harigeethamneethu02@gmail.com>
2023-04-22 20:26:37 +0530

    created a contact app

--- contact/src/components/App.js
+++ contact/src/components/App.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
-
+import {uuid} from "uuidv4"
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+const[contacts,setContacts] = useState([])
+
+const addContactHandler=(contact) =>{
+  console.log(contact);
+  setContacts([...contacts,{id: uuid(),...contacts}]);
+};
+
+const removeContactHandler = (id) =>{
+  const newContactList =contacts.filter((contacts)=>{
+    return contact.id !== id;
+  }
+  )
+}
+
+useEffect(()=>{
+ const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+ if(retriveContacts) setContacts(retriveContacts);
+},[]);
+
+useEffect(()=>{
+  localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+},[contacts]);
+
+
   return (
-    <div>
+    <div className='ui container'>
     <Header/>
-    <AddContact/>
-    <ContactList/>
+    <AddContact addContactHandler={addContactHandler}/>
+    <ContactList contacts={contacts} getContactID={removeContactHandler}/>
     </div>
   );
 }
